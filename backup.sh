02951f4f2d05f358ac2c0ab3342bc5c3710e3637
ming <kahikara83@gmail.com>
2026-03-12 17:25:14 +0100

    wip: save local changes before rebase

--- backup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-ts=$(date +"%Y-%m-%d_%H-%M-%S")
-tar \
-  --exclude='node_modules' \
-  --exclude='src-tauri/target' \
-  --exclude='dist' \
-  --exclude='.git' \
-  --exclude='*.tar.gz' \
-  -czf "termina-ssh-snapshot_$ts.tar.gz" .
